6141e1fb33c3a81f520f7afe70bf763e68b7489a
anhasamea <anhasamea707@gmail.com>
2022-10-06 20:47:28 +0600

    price-club

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = () => { 
+    const routes = [
+        {id:1,name: 'Home', path:'/home'},
+        {id:2,name: 'Products', path:'/home'},
+        {id:3,name: 'Orders', path:'/home'},
+        {id:4,name: 'Contacts', path:'/home'},
+        {id:5,name: 'About', path:'/home'}
+    ]
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NavBar;
